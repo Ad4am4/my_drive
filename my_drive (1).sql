73fc4deb9143785175fe3ec67bdd9fca88bfc9e1
Ad4am4 <88541528+Ad4am4@users.noreply.github.com>
2026-04-24 09:25:37 +0700

    Add files via upload

---
+++ my_drive (1).sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2026 at 03:21 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `my_drive`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `files`
+--
+
+CREATE TABLE `files` (
+  `id` int(11) NOT NULL,
+  `file_name` varchar(255) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `upload_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `files`
+--
+
+INSERT INTO `files` (`id`, `file_name`, `file_path`, `upload_time`) VALUES
+(1, 'VID_20250608201956.mp4', 'uploads/VID_20250608201956.mp4', '2026-03-28 04:13:15'),
+(2, 'VID20250805122346.mp4', 'uploads/VID20250805122346.mp4', '2026-03-28 04:24:22'),
+(3, 'VID20240823151712.mp4', 'uploads/VID20240823151712.mp4', '2026-03-28 04:27:27'),
+(4, 'ngrok_recovery_codes.txt', 'uploads/ngrok_recovery_codes.txt', '2026-03-28 04:37:42'),
+(5, 'bb112c57fc910caf77d2587bee052db0.mp4', 'uploads/bb112c57fc910caf77d2587bee052db0.mp4', '2026-03-28 05:33:05'),
+(6, 'VID20251224104009.mp4', 'uploads/VID20251224104009.mp4', '2026-03-28 12:44:46'),
+(7, 'ngrok-v3-stable-windows-amd64.zip', 'uploads/ngrok-v3-stable-windows-amd64.zip', '2026-03-28 12:56:35'),
+(8, 'PB5MAT+Materi 5 - QA.pdf', 'uploads/PB5MAT+Materi 5 - QA.pdf', '2026-03-31 05:19:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'a@gmail.com', '$2y$10$jCEc4NRM6z6xy3aIF6oTdOZbU5wUm6K3iIAidOqlrSFXgKp2Zl736');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

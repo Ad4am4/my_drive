a05392e3163983a5bb4ee95a908a57654a7d6141
Ad4am4 <88541528+Ad4am4@users.noreply.github.com>
2026-05-15 09:19:56 +0700

    Update database connection credentials

--- my_drive - Copy/db.php
+++ my_drive - Copy/db.php
@@ -1,12 +1,11 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "my_drive";
+$user = "admin_drive";        // <-- Ubah menjadi username baru
+$pass = "Securedrive123";      // <-- Masukkan password yang kamu buat tadi
+$db = "my_drive";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
-?>
